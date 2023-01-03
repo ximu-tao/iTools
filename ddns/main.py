43895cd28f8fe5a92c4900a15de944ed14cbbbb1
ximu-tao <me_litao@outlook.com>
2023-01-03 13:59:30 +0800

    ddns: 添加 CloudFlare API 示例代码

--- ddns/main.py
+++ ddns/main.py
@@ -1,6 +1,13 @@
 from api import Godaddy
+from api import CloudFlare
 
 gApi = Godaddy( KEY = 'xxxxxxxxxxxxxxxxxxxx', SECRET='xxxxxxxxxxxxxxxxxxx', DOMAIN='domain.com' )
 
 gApi.cname( '@' , 'baidu.com' )
 
+
+cfApi = CloudFlare( API_TOKEN='xxxxxxxxxxxxxxxxxxxx' , Zone_ID='xxxxxxxxxxxxxxxxxxxx', DOMAIN='domain.com')
+
+cfApi.update_dns( TYPE='A' , NAME='qwe' , VALUE='192.168.7.1'  )
+
+cfApi.a( '@' , VALUE='192.167.1.1')
