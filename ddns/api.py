713daa3046f52d4ce9670f5a58163069efb6b155
ximu-tao <me_litao@outlook.com>
2023-01-03 13:57:51 +0800

    ddns: 更新

--- ddns/api.py
+++ ddns/api.py
@@ -6,13 +6,13 @@ class API:
     def __init__(self):
         pass
 
-    def cname(self , NAME , VALUE , TTL=None):
+    def cname(self, NAME, VALUE, TTL=None):
         pass
 
-    def a(self , NAME , VALUE , TTL=None):
+    def a(self, NAME, VALUE, TTL=None):
         pass
 
-    def aaaa(self , NAME , VALUE , TTL=None):
+    def aaaa(self, NAME, VALUE, TTL=None):
         pass
 
     def txt(self):
@@ -47,23 +47,23 @@ class Godaddy(API):
         self.TTL = TTL
         self.DOMAIN = DOMAIN
 
-    def update_dns(self , TYPE , NAME, VALUE, TTL ):
+    def update_dns(self, TYPE, NAME, VALUE, TTL=60 * 60):
         GOD_ADDY_API_URL = "https://api.godaddy.com/v1/domains/{}/records/{}/{}".format(self.DOMAIN, TYPE, NAME)
-        data = json.dumps([{"data": VALUE, "name": NAME, "ttl": self.TTL, "type": TYPE}]).encode('utf-8')
+        data = json.dumps([{"data": VALUE, "name": NAME, "ttl": TTL, "type": TYPE}]).encode('utf-8')
         # print( GOD_ADDY_API_URL )
         req = request.Request(url=GOD_ADDY_API_URL, data=data, headers=self.HEADERS, method='PUT')
         with request.urlopen(req) as response:
             pass
         return VALUE if not response.read().decode('utf-8') else VALUE
 
-    def cname(self , NAME , VALUE , TTL=None):
-        self.update_dns( 'CNAME' , NAME , VALUE , TTL= TTL if TTL else self.TTL)
+    def cname(self, NAME, VALUE, TTL=None):
+        self.update_dns('CNAME', NAME, VALUE, TTL=TTL if TTL else self.TTL)
 
-    def a(self , NAME , VALUE , TTL=None):
-        self.update_dns( 'A' , NAME , VALUE , TTL= TTL if TTL else self.TTL)
+    def a(self, NAME, VALUE, TTL=None):
+        self.update_dns('A', NAME, VALUE, TTL=TTL if TTL else self.TTL)
 
-    def aaaa(self , NAME , VALUE , TTL=None):
-        self.update_dns( 'AAAA' , NAME , VALUE , TTL= TTL if TTL else self.TTL)
+    def aaaa(self, NAME, VALUE, TTL=None):
+        self.update_dns('AAAA', NAME, VALUE, TTL=TTL if TTL else self.TTL)
 
 
 class CloudFlare(API):
